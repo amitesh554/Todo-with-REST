d4b5f6d7f97be3885faeb8973db7b382f706c61f
Amitesh Rai <abhiapor543@gmail.com>
2023-11-18 23:47:59 +0530

    Added some requirements for hosting

--- optimite/settings.py
+++ optimite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3wis@(m@b89@9$vqlodjz-t3-f+55_9!n%ra+11^--*elk@n2p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
